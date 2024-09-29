0175bec45277ee31213503bde77a1f57d4592d85
reinhym <reinhym@gmail.com>
2024-09-30 08:50:06 +1000

    Add project files.

---
+++ Xamarin2_TravelRecordApp/Xamarin2_TravelRecordApp/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Xamarin2_TravelRecordApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Clicked(object sender, EventArgs e)
+        {
+            bool isEmailEmpty = string.IsNullOrEmpty(EmailEntry.Text);
+            bool isPasswordEmpty = string.IsNullOrEmpty(PasswordEntry.Text);
+
+            if (isEmailEmpty || isPasswordEmpty)
+            {
+                return;
+            }
+        }
+    }
+}
